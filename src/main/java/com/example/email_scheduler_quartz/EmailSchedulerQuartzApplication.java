fb7626536632d99d722330e016fa7b68aefc9bb9
Bryan Leow K.H <bryankhleow@gmail.com>
2025-02-18 13:42:52 +0800

    Initial commit

---
+++ src/main/java/com/example/email_scheduler_quartz/EmailSchedulerQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.example.email_scheduler_quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSchedulerQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSchedulerQuartzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/email_scheduler_quartz/EmailSchedulerQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.email_scheduler_quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSchedulerQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
