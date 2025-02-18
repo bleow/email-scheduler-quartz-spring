af35af2c05926526b6efef346c0c53596424c05e
Bryan Leow K.H <bryankhleow@gmail.com>
2025-02-18 19:32:35 +0800

    feat: implement sending mail in EmailService

--- src/main/java/com/example/email_scheduler_quartz/service/EmailService.java
+++ src/main/java/com/example/email_scheduler_quartz/service/EmailService.java
@@ -1,9 +1,39 @@
 package com.example.email_scheduler_quartz.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+
 @Service
+@Slf4j
+@AllArgsConstructor
 public class EmailService {
-    public void send(String mailTo, String subject, String body) {
+    private JavaMailSender mailSender;
+
+    private MailProperties mailProperties;
+
+    public void send(String mailTo, String subject, String body)  {
+        String mailFrom = mailProperties.getUsername();
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, StandardCharsets.UTF_8.toString());
+
+            messageHelper.setFrom(mailFrom);
+            messageHelper.setTo(mailTo);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(body, true);
+
+            mailSender.send(message);
+
+        } catch (MessagingException e) {
+            log.error("Exception occurred when constructing email:", e);
+        }
     }
 }
